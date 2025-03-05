5521f2666e978784eba635fa2438dd64db4df94f
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-03-05 12:45:43 -0300

    fix: button link bug

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -5,19 +5,10 @@ import { NavLink } from 'react-router';
 const Button = (props) => {
    return (
       <>
-         {
-            props.to ?
-               <div className={style.navlink} title={props.title}>
-                  {props.to && <NavLink to={props.to}>{props.children}</NavLink>}
-               </div>
-               : <div className={style.button} title={props.title}>
-                  {props.href && <a href={props.href} target="_blank" className={style.customIcon}> {props.children}</a>}
-                  {props.to && <NavLink to={props.to} className={style.customIcon + " teste"}>{props.children}</NavLink>}
-                  {props.onlyIcon && <span className={style.customIcon}>{props.children}</span>}
-               </div>
-         }
+         {props.to && <div className={style.navlink} title={props.title}><NavLink to={props.to}>{props.children}</NavLink></div>}
+         {props.href && <a className={style.button} href={props.href} target="_blank" title={props.title}><span className={style.customIcon}> {props.children}</span></a>}
+         {props.onlyIcon && <div className={style.button} title={props.title}><span className={style.customIcon}>{props.children}</span></div>}
       </>
-
    )
 }
 
