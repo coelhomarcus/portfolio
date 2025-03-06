fdbdaed17c3a7ec3a218241a021b5d460e44d2f4
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-03-06 15:07:31 -0300

    fix: navbutton component

--- src/Components/NavButton/NavButton.jsx
+++ src/Components/NavButton/NavButton.jsx
@@ -8,7 +8,7 @@ const NavButton = (props) => {
       <>
          {props.to && <NavLink to={props.to} className={style.navlink}><span><h1>{props.h1}</h1><p>{props.p}</p></span></NavLink>}
          {(props.href && !props.cert) && <a href={props.href} target="_blank" className={style.navlink}><span><h1>{props.h1}</h1><p>{props.p}</p></span></a>}
-         {props.cert && <a href={props.href} target="_blank" className={style.navlink}>
+         {props.cert && <a href={props.href} target="_blank" className={style.certlink}>
             <span>
                <h1>{props.h1}</h1>
                <p>Issued by: {props.p}</p>
