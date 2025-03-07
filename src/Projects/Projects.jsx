0106ea6ff3061561738d7a734cf2a5a890634341
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-03-07 00:32:25 -0300

    fix: change folder name

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -15,10 +15,10 @@ const Projects = () => {
          </div>
          <Text h2="Experimental / Prototypes" />
          <div className='gridColumn'>
-            <Card img="./projects/bash.webp" github="https://github.com/coelhomarcus/bunnybash" link="https://bash.coelhomarcus.com" title="BunnyBash" desc="A website that simulates a terminal" />
-            <Card img="./projects/catype.webp" github="https://github.com/coelhomarcus/catype" link="https://catype.coelhomarcus.com" title="Catype" desc="Typing page prototype" />
-            <Card img="./projects/mailman.webp" link="https://bakanull.itch.io/mailman" title="Mailman" desc="A prototype of a retro horror game" />
-            <Card img="./projects/choice.webp" link="https://bakanull.itch.io/right-choice" title="Right Choice" desc="My first prototype of a retro horror game" />
+            <Card img="./projectsimg/bash.webp" github="https://github.com/coelhomarcus/bunnybash" link="https://bash.coelhomarcus.com" title="BunnyBash" desc="A website that simulates a terminal" />
+            <Card img="./projectsimg/catype.webp" github="https://github.com/coelhomarcus/catype" link="https://catype.coelhomarcus.com" title="Catype" desc="Typing page prototype" />
+            <Card img="./projectsimg/mailman.webp" link="https://bakanull.itch.io/mailman" title="Mailman" desc="A prototype of a retro horror game" />
+            <Card img="./projectsimg/choice.webp" link="https://bakanull.itch.io/right-choice" title="Right Choice" desc="My first prototype of a retro horror game" />
          </div>
       </div>
    )
