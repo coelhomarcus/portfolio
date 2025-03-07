5712bec3580d861501cac01d44a0e52ce802e450
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-03-07 13:07:57 -0300

    add new knowledge

--- src/Stack/DevStack/DevStack.jsx
+++ src/Stack/DevStack/DevStack.jsx
@@ -8,7 +8,7 @@ import Text from '../../Components/Text/Text';
 import { IoLogoJavascript, IoLogoHtml5, IoLogoCss3, IoLogoPython, IoLogoReact, IoLogoNodejs } from "react-icons/io5";
 import { FaJava, FaSwift, FaFigma, FaBrave, FaGithub, FaUnity } from "react-icons/fa6";
 import { VscVscode } from "react-icons/vsc";
-import { SiInsomnia, SiTodoist, SiMysql, SiMongodb, SiXcode } from "react-icons/si";
+import { SiInsomnia, SiTodoist, SiMysql, SiMongodb, SiXcode, SiBlender, SiVite, SiReactrouter } from "react-icons/si";
 
 const DevStack = () => {
    return (
@@ -18,7 +18,6 @@ const DevStack = () => {
          <Text h1="Development Stack" p="All of the apps, tools, languages and services I use to build my projects." />
          <div className='flexColumn'>
             <Text h2="Programming & Typesetting Languages" />
-
             <div className='flexRow'>
                <Button onlyIcon title="JavaScript"><IoLogoJavascript /></Button>
                <Button onlyIcon title="HTML"><IoLogoHtml5 /></Button>
@@ -34,7 +33,9 @@ const DevStack = () => {
 
             <div className='flexRow'>
                <Button onlyIcon title="React"><IoLogoReact /></Button>
+               <Button onlyIcon title="React Router"><SiReactrouter /></Button>
                <Button onlyIcon title="NodeJs"><IoLogoNodejs /></Button>
+               <Button onlyIcon title="Vite"><SiVite /></Button>
             </div>
          </div>
 
@@ -49,12 +50,13 @@ const DevStack = () => {
          <div className='flexColumn'>
             <Text h2="Apps" />
             <div className='flexRow'>
-               <Button onlyIcon title="VSCode"><VscVscode /></Button>
-               <Button onlyIcon title="Figma"><FaFigma /></Button>
-               <Button onlyIcon title="Insomnia"><SiInsomnia /></Button>
-               <Button onlyIcon title="Unity"><FaUnity /></Button>
-               <Button onlyIcon title="Brave"><FaBrave /></Button>
                <Button onlyIcon title="GitHub"><FaGithub /></Button>
+               <Button onlyIcon title="VSCode"><VscVscode /></Button>
+               <Button onlyIcon title="Insomnia"><SiInsomnia /></Button>
+               <Button onlyIcon title="Figma"><FaFigma /></Button>
+               <Button onlyIcon title="Unity"><FaUnity /></Button>
+               <Button onlyIcon title="Blender"><SiBlender /></Button>
+               <Button onlyIcon title="Brave"><FaBrave /></Button>
                <Button onlyIcon title="Todoist"><SiTodoist /></Button>
                <Button onlyIcon title="XCode"><SiXcode /></Button>
             </div>
