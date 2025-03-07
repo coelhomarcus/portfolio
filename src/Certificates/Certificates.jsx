674f5e2a97379bf8fc996c764ea6efd9f439f157
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-03-07 11:46:54 -0300

    update certificates

--- src/Certificates/Certificates.jsx
+++ src/Certificates/Certificates.jsx
@@ -10,6 +10,12 @@ const Certificates = () => {
          <Text h1="Certificates" p="My certificates" />
          <div className="options">
             <NavButton
+               cert="13f9b32b"
+               date="mar/2025"
+               href="https://www.origamid.com/certificate/13f9b32b"
+               h1="Complete React"
+               p="Origamid" />
+            <NavButton
                cert="b1ee7546"
                date="feb/2025"
                href="https://www.origamid.com/certificate/b1ee7546"
@@ -22,13 +28,25 @@ const Certificates = () => {
                h1="Code Versioning"
                p="PUCRS" />
             <NavButton
-               cert="656dc90b-3fd8-46a6-8a2c-ca59ac120003"
-               date="dec/2023"
-               href="https://lms.hackatruck.com.br/mod/simplecertificate/verify.php"
-               h1="Algorithms and Object-Oriented Programming with Swift"
-               p="Instituto Eldorado (HACKATRUCK)" />
+               cert="8e8ad089-6179-4b5b-acd7-ede483cdd348"
+               date="dec/2024"
+               href="https://ticemtrilhas.org.br/certificate-validation?code=8e8ad089-6179-4b5b-acd7-ede483cdd348"
+               h1="Fundamentals of Web Development"
+               p="TIC em Trilhas"
+            />
             <NavButton
-
+               cert="731272b6-5951-4b31-928f-5ae202e4b56c"
+               date="oct/2024"
+               href="https://ticemtrilhas.org.br/certificate-validation?code=731272b6-5951-4b31-928f-5ae202e4b56c"
+               h1="Introduction to Game Development - Unity"
+               p="TIC em trilhas" />
+            <NavButton
+               cert="29d9cad9-266c-4079-b076-98557f861e37"
+               date="oct/2024"
+               href="https://ticemtrilhas.org.br/certificate-validation?code=29d9cad9-266c-4079-b076-98557f861e37"
+               h1="3D Modeling for Games - Blender"
+               p="TIC em trilhas" />
+            <NavButton
                cert="65c60c03-904c-4550-8a66-5670ac120004"
                date="feb/2024"
                href="https://lms.hackatruck.com.br/mod/simplecertificate/verify.php"
@@ -36,12 +54,11 @@ const Certificates = () => {
                p="Instituto Eldorado (HACKATRUCK)"
             />
             <NavButton
-               cert="8e8ad089-6179-4b5b-acd7-ede483cdd348"
-               date="dec/2024"
-               href="https://ticemtrilhas.org.br/certificate-validation?code=8e8ad089-6179-4b5b-acd7-ede483cdd348"
-               h1="Fundamentals of Web Development"
-               p="TIC em Trilhas"
-            />
+               cert="656dc90b-3fd8-46a6-8a2c-ca59ac120003"
+               date="dec/2023"
+               href="https://lms.hackatruck.com.br/mod/simplecertificate/verify.php"
+               h1="Algorithms and Object-Oriented Programming with Swift"
+               p="Instituto Eldorado (HACKATRUCK)" />
          </div>
       </div>
    )
