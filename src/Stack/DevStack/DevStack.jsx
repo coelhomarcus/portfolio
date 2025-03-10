09b601ba3028ab52577c74cc48bb79950f38d321
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-03-10 10:10:14 -0300

    fix: grid

--- src/Stack/DevStack/DevStack.jsx
+++ src/Stack/DevStack/DevStack.jsx
@@ -8,7 +8,8 @@ import Text from '../../Components/Text/Text';
 import { IoLogoJavascript, IoLogoHtml5, IoLogoCss3, IoLogoPython, IoLogoReact, IoLogoNodejs } from "react-icons/io5";
 import { FaJava, FaSwift, FaFigma, FaBrave, FaGithub, FaUnity } from "react-icons/fa6";
 import { VscVscode } from "react-icons/vsc";
-import { SiInsomnia, SiTodoist, SiMysql, SiMongodb, SiXcode, SiBlender, SiVite, SiReactrouter } from "react-icons/si";
+import { SiInsomnia, SiTodoist, SiMysql, SiMongodb, SiXcode, SiBlender, SiVite, SiReactrouter, SiTypescript, SiGodotengine } from "react-icons/si";
+import { TbBrandCSharp } from "react-icons/tb";
 
 const DevStack = () => {
    return (
@@ -20,11 +21,13 @@ const DevStack = () => {
             <Text h2="Programming & Typesetting Languages" />
             <div className='flexRow'>
                <Button onlyIcon title="JavaScript"><IoLogoJavascript /></Button>
+               <Button onlyIcon title="TypeScript"><SiTypescript /></Button>
                <Button onlyIcon title="HTML"><IoLogoHtml5 /></Button>
                <Button onlyIcon title="CSS"><IoLogoCss3 /></Button>
                <Button onlyIcon title="Python"><IoLogoPython /></Button>
                <Button onlyIcon title="Java"><FaJava /></Button>
                <Button onlyIcon title="Swift"><FaSwift /></Button>
+               <Button onlyIcon title="C#"><TbBrandCSharp /></Button>
             </div>
          </div>
 
@@ -55,6 +58,7 @@ const DevStack = () => {
                <Button onlyIcon title="Insomnia"><SiInsomnia /></Button>
                <Button onlyIcon title="Figma"><FaFigma /></Button>
                <Button onlyIcon title="Unity"><FaUnity /></Button>
+               <Button onlyIcon title="Godot"><SiGodotengine /></Button>
                <Button onlyIcon title="Blender"><SiBlender /></Button>
                <Button onlyIcon title="Brave"><FaBrave /></Button>
                <Button onlyIcon title="Todoist"><SiTodoist /></Button>
