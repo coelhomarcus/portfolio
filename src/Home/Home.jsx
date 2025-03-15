697e8781ee62907697b89a109018ea05913a838d
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-03-15 12:46:04 -0300

    add images

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,6 +4,17 @@ import Head from '../Head/Head'
 import Text from '../Components/Text/Text'
 import Button from '../Components/Button/Button'
 
+import Stack from '../ReactBits/Stack/Stack'
+
+const images = [
+   { id: 1, img: "/img/Pessoal/hackatruck.webp" },
+   { id: 2, img: "./img/Pessoal/cafundis.webp" },
+   { id: 3, img: "/img/Pessoal/nos.webp" },
+   { id: 4, img: "./img/Pessoal/marcus.webp" },
+
+];
+
+
 const Home = () => {
    return (
       <div className="main">
@@ -17,7 +28,13 @@ const Home = () => {
          <Text h2="Professional Experience" h3="🦎 Exception Jr | Jul 2024 - Present" p="Junior Enterprise at the Federal University of Southern and Southeastern Pará, working as a Full Stack Developer." />
          <Text h3="🐴 Steed | Jul 2024 - Present" p="A startup specializing in website development, where I serve as the Founder and Full Stack Developer." />
          <div>
-
+            <Stack
+               randomRotation={true}
+               sensitivity={180}
+               sendToBackOnClick={false}
+               cardDimensions={{ width: 300, height: 300 }}
+               cardsData={images}
+            />
          </div>
       </div>
    )
--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Text from '../Components/Text/Text';
 import Head from '../Head/Head';
-import NavButton from '../Components/NavButton/NavButton';
 import Card from '../Components/Card/Card';
 
 const Projects = () => {
@@ -12,16 +11,16 @@ const Projects = () => {
          <div className="options">
             <Text h2="Primary Projects" p="Real projects" />
             <div className='gridColumn'>
-               <Card img="./projectsimg/sos.webp" link="https://www.youtube.com/shorts/0fSoHjAadas" title="SOS Queimadas" desc="IOS App for Wildfire Alert and Control" />
-               <Card img="./projectsimg/canaa.webp" link="https://www.youtube.com/shorts/0fSoHjAadas" title="Conecta Canaã" desc="Website for local services and community information in Canaã - PA" />
+               <Card img="./img/Projects/sos.webp" link="https://www.youtube.com/shorts/0fSoHjAadas" title="SOS Queimadas" desc="IOS App for Wildfire Alert and Control" />
+               <Card img="./img/Projects/canaa.webp" link="https://www.youtube.com/shorts/0fSoHjAadas" title="Conecta Canaã" desc="Website for local services and community information in Canaã - PA" />
             </div>
          </div>
          <Text h2="Experimental / Prototypes" p="Projects I created for learning and curiosity" />
          <div className='gridColumn'>
-            <Card img="./projectsimg/bash.webp" github="https://github.com/coelhomarcus/bunnybash" link="https://bash.coelhomarcus.com" title="BunnyBash" desc="A website that simulates a terminal" />
-            <Card img="./projectsimg/catype.webp" github="https://github.com/coelhomarcus/catype" link="https://catype.coelhomarcus.com" title="Catype" desc="Typing page prototype" />
-            <Card img="./projectsimg/mailman.webp" link="https://bakanull.itch.io/mailman" title="Mailman" desc="A prototype of a retro horror game" />
-            <Card img="./projectsimg/choice.webp" link="https://bakanull.itch.io/right-choice" title="Right Choice" desc="My first prototype of a retro horror game" />
+            <Card img="./img/Projects/bash.webp" github="https://github.com/coelhomarcus/bunnybash" link="https://bash.coelhomarcus.com" title="BunnyBash" desc="A website that simulates a terminal" />
+            <Card img="./img/Projects/catype.webp" github="https://github.com/coelhomarcus/catype" link="https://catype.coelhomarcus.com" title="Catype" desc="Typing page prototype" />
+            <Card img="./img/Projects/mailman.webp" link="https://bakanull.itch.io/mailman" title="Mailman" desc="A prototype of a retro horror game" />
+            <Card img="./img/Projects/choice.webp" link="https://bakanull.itch.io/right-choice" title="Right Choice" desc="My first prototype of a retro horror game" />
          </div>
       </div>
    )
