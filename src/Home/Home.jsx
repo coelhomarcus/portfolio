876f4244f7ae1ec367ac65c89ca6be7a167cf21f
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-03-15 13:14:43 -0300

    remove stack

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -4,15 +4,12 @@ import Head from '../Head/Head'
 import Text from '../Components/Text/Text'
 import Button from '../Components/Button/Button'
 
-import Stack from '../ReactBits/Stack/Stack'
-
-const images = [
-   { id: 1, img: "/img/Pessoal/hackatruck.webp" },
-   { id: 2, img: "./img/Pessoal/cafundis.webp" },
-   { id: 3, img: "/img/Pessoal/nos.webp" },
-   { id: 4, img: "./img/Pessoal/marcus.webp" },
-
-];
+// const images = [
+//    { id: 1, img: "/img/Pessoal/hackatruck.webp" },
+//    { id: 2, img: "./img/Pessoal/cafundis.webp" },
+//    { id: 3, img: "/img/Pessoal/nos.webp" },
+//    { id: 4, img: "./img/Pessoal/marcus.webp" },
+// ];
 
 
 const Home = () => {
@@ -28,13 +25,6 @@ const Home = () => {
          <Text h2="Professional Experience" h3="🦎 Exception Jr | Jul 2024 - Present" p="Junior Enterprise at the Federal University of Southern and Southeastern Pará, working as a Full Stack Developer." />
          <Text h3="🐴 Steed | Jul 2024 - Present" p="A startup specializing in website development, where I serve as the Founder and Full Stack Developer." />
          <div>
-            <Stack
-               randomRotation={true}
-               sensitivity={180}
-               sendToBackOnClick={false}
-               cardDimensions={{ width: 300, height: 300 }}
-               cardsData={images}
-            />
          </div>
       </div>
    )
