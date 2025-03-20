3317ded872b18582a65fbdafdbec72621a7cc267
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-03-19 22:08:25 -0300

    fix: background opacity

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,63 @@
-import React from 'react'
-import './global.css'
+import React from "react";
+import "./global.css";
 import { Routes, Route, useLocation } from "react-router";
 
-import Sidebar from './Sidebar/Sidebar'
-import Home from './Home/Home'
-import Contact from './Contact/Contact'
-import Stack from './Stack/Stack'
-import DeskSetup from './Stack/DeskSetup/DeskSetup'
-import DevStack from './Stack/DevStack/DevStack'
-import VSCode from './Stack/VSCode/VSCode'
-import Certificates from './Certificates/Certificates'
-import Projects from './Projects/Projects';
-import Stuff from './Stuff/Stuff';
-import NotFound from './NotFound/NotFound';
+import Sidebar from "./Sidebar/Sidebar";
+import Home from "./Home/Home";
+import Contact from "./Contact/Contact";
+import Stack from "./Stack/Stack";
+import DeskSetup from "./Stack/DeskSetup/DeskSetup";
+import DevStack from "./Stack/DevStack/DevStack";
+import VSCode from "./Stack/VSCode/VSCode";
+import Certificates from "./Certificates/Certificates";
+import Projects from "./Projects/Projects";
+import Stuff from "./Stuff/Stuff";
+import NotFound from "./NotFound/NotFound";
 
 //other
-import Aurora from './ReactBits/Aurora/Aurora';
+import Aurora from "./ReactBits/Aurora/Aurora";
 
 function App() {
-   const [isLoaded, setIsLoaded] = React.useState(false);
-   const location = useLocation();
+  const [isLoaded, setIsLoaded] = React.useState(false);
+  const location = useLocation();
 
-   React.useEffect(() => {
-      const timer = setTimeout(() => {
-         setIsLoaded(true);
-      }, 500);
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoaded(true);
+    }, 500);
 
-      return () => clearTimeout(timer);
-   }, []);
+    return () => clearTimeout(timer);
+  }, []);
 
-   React.useEffect(() => scrollTo(top), [location]);
+  React.useEffect(() => scrollTo(top), [location]);
 
-   return (
-      <div className='container'>
-         {isLoaded ? <Aurora
-            colorStops={["#00D8FF", "#7CFF67", "#00D8FF"]}
-            blend={1}
-            amplitude={1}
-            speed={1}
-         /> : null}
-         <div className='content'>
-            <Sidebar />
-            <Routes>
-               <Route path="/" element={<Home />} />
-               <Route path="contact" element={<Contact />} />
-               <Route path="stack" element={<Stack />} />
-               <Route path="stack/desk-setup" element={<DeskSetup />} />
-               <Route path="stack/dev-stack" element={<DevStack />} />
-               <Route path="stack/vs-setup" element={<VSCode />} />
-               <Route path="certificates" element={<Certificates />} />
-               <Route path="projects" element={<Projects />} />
-               <Route path="stuff" element={<Stuff />} />
-               <Route path="*" element={<NotFound />} />
-            </Routes>
-         </div>
+  return (
+    <div className="container">
+      {isLoaded ? (
+        <Aurora
+          colorStops={["#00D8FF", "#11FFBD", "#00D8FF"]}
+          blend={0.8}
+          amplitude={0.8}
+          speed={0.5}
+        />
+      ) : null}
+      <div className="content">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="stack" element={<Stack />} />
+          <Route path="stack/desk-setup" element={<DeskSetup />} />
+          <Route path="stack/dev-stack" element={<DevStack />} />
+          <Route path="stack/vs-setup" element={<VSCode />} />
+          <Route path="certificates" element={<Certificates />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="stuff" element={<Stuff />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
-   )
+    </div>
+  );
 }
 
-export default App
+export default App;
