7be1c1556d9d8e54cac89d2d7fa7be256c1cd41d
Marcus Coelho <marcusrangelcoelho@gmail.com>
2025-03-25 23:02:18 -0300

    feat: discord iframe

--- src/Stuff/Stuff.jsx
+++ src/Stuff/Stuff.jsx
@@ -1,26 +1,49 @@
-import React from 'react'
-import Head from '../Head/Head'
-import Text from '../Components/Text/Text'
-import Button from '../Components/Button/Button'
+import React from "react";
+import Head from "../Head/Head";
+import Text from "../Components/Text/Text";
+import Button from "../Components/Button/Button";
 
 import { SiMonkeytype } from "react-icons/si";
 import { FaSteam, FaYoutube } from "react-icons/fa6";
 
 const Stuff = () => {
-   return (
-      <div className='main'>
-         <Head title="Outros" />
-         <Text h1="Outros" p="Algumas coisas que gosto :D" />
-         <div className='flexRow'>
-            <Button href="https://monkeytype.com/profile/coelhomarcus" title="MonkeyType"><SiMonkeytype /></Button>
-            <Button href="https://steamcommunity.com/id/bakanull/" title="My Steam"><FaSteam /></Button>
-            <Button href="https://www.youtube.com/@bakanull" title="My Channel"><FaYoutube /></Button>
-         </div>
-         <Text h2="Jogos Favoritos" pArray={["• Cyberpunk 2077", "• Elden Ring", "• Dark Souls III"]} />
-         <Text h2="Mangás Favoritos" pArray={["• Kaiji", "• Berserk", "• 20th Century Boys", "• Chainsaw Man", "• Death Note"]} />
-         <Text h2="Cantor Favorito" p="• Enygma 🐐" />
-      </div>
-   )
-}
+  return (
+    <div className="main">
+      <Head title="Outros" />
+      <Text h1="Outros" />
+      <Text
+        h2="Sites & Rede Sociais"
+        p="Alguns sites/redes sociais que utilizo"
+      />
 
-export default Stuff
+      <div className="flexRow">
+        <Button
+          href="https://monkeytype.com/profile/coelhomarcus"
+          title="MonkeyType"
+        >
+          <SiMonkeytype />
+        </Button>
+        <Button href="https://steamcommunity.com/id/bakanull/" title="My Steam">
+          <FaSteam />
+        </Button>
+        <Button href="https://www.youtube.com/@bakanull" title="My Channel">
+          <FaYoutube />
+        </Button>
+      </div>
+      <Text
+        h2="Bits & Bugs"
+        p="Nossa comunidade no discord feita por devs para devs, venha participar!"
+      />
+      <iframe
+        src="https://discord.com/widget?id=1353895304423936092&theme=dark"
+        width="auto"
+        height="450"
+        allowtransparency="true"
+        frameborder="0"
+        sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
+      ></iframe>
+    </div>
+  );
+};
+
+export default Stuff;
